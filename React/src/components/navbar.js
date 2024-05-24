c722b57618ea20fa4d75e09525a8f46b52647a7d
DEVANSARVANAN <devan.saravanan1502@gmail.com>
2024-05-24 15:13:15 -0700

    feat:Navbar Updated

--- React/src/components/navbar.js
+++ React/src/components/navbar.js
@@ -3,55 +3,6 @@
 
 
 
-// import React from 'react';
-// // import { useNavigation } from '@react-navigation/native';
-
-// import { useNavigate } from 'react-router-dom';
-
-// function Navigation() {
-
-//     const navigate = useNavigate();
-//   return (
-//     <div>
-//       {/* Navigation Bar */}
-//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//         <div className="container">
-//           <p className="navbar-brand" >Payment App</p>
-//           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarNav">
-//             <ul className="navbar-nav ml-auto"> {/* Add ml-auto class here */}
-//               <li className="nav-item">
-//                 <p className="nav-link" onClick={() => navigate('../home', { replace: true })}>Home</p>
-//               </li>
-//               <li className="nav-item">
-//                 <p className="nav-link" onClick={() => navigate('../profile', { replace: true })}>Profile</p>
-//               </li>
-//               <li className="nav-item">
-//                 <p className="nav-link" onClick={() => navigate('../payment', { replace: true })}>Payment</p>
-//               </li>
-//               <li className="nav-item active">
-//                 <p className="nav-link"onClick={() => navigate('../notification', { replace: true })}>Notifications </p>
-//               </li>
-//               <li className="nav-item">
-//                 <p className="nav-link" onClick={() => navigate('../settings', { replace: true })}>Settings</p>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Navigation;
-
-
-
-
-
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navigation.css'; // Import custom CSS file for additional styling
